a426f3b56731321e95b7dc7ec8e2bd2385b9cd6f
FKI <FKI@DESKTOP-5PLH4OK>
2024-10-04 10:42:37 -0300

    Agregar archivos de proyecto.

---
+++ Definitivo CUDI2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Definitivo_CUDI2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // Abre Google en el navegador predeterminado
+            Process.Start(new ProcessStartInfo("https://www.google.com") { UseShellExecute = true });
+        }
+    }
+}
